68f517ac6f37dcb5a555b2859acc85a1e0bcf0d8
novydhatika <novydhatika3011@gmail.com>
2017-03-28 17:55:08 -0700

    CodeIgniter
    CodeIgniter

---
+++ codeigniter_alphaa/application/views/about.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html lang="">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Novy Dhatika Website</title>
+
+		<!-- Bootstrap CSS -->
+		<link rel="stylesheet" href="<?php echo base_url('') ?>assets/css/bootstrap.min.css">
+
+		<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+		<!--[if lt IE 9]>
+			<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.2/html5shiv.min.js"></script>
+			<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+		<![endif]-->
+	</head>
+	<body>
+		<div class="container-fluid">
+				<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+						<nav class="navbar navbar-default" role="navigation">
+							<div class="container-fluid">
+								<!-- Brand and toggle get grouped for better mobile display -->
+								<div class="navbar-header">
+								<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+										<span class="sr-only">Toggle navigation</span>
+										<span class="icon-bar"></span>
+										<span class="icon-bar"></span>
+										<span class="icon-bar"></span>
+									</button>
+									<ul class="nav navbar-nav">
+										<li class="active"><a href="<?php echo site_url('about') ?>">Biodata </a></li>
+									</ul>
+									
+								</div>
+						
+								<!-- Collect the nav links, forms, and other content for toggling -->
+								<div class="collapse navbar-collapse navbar-ex1-collapse">
+									<a class="navbar-brand" href="<?php echo site_url('Pegawai') ?>">Biodata Pegawai</a>
+									<ul class="nav navbar-nav navbar-right">
+										<li class="dropdown">
+											<a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
+											<ul class="dropdown-menu">
+												<li><a href="#">Action</a></li>
+												<li><a href="#">Another action</a></li>
+												<li><a href="#">Something else here</a></li>
+												<li><a href="#">Separated link</a></li>
+											</ul>
+										</li>
+									</ul>
+								</div><!-- /.navbar-collapse -->
+						</div>
+						</nav>
+
+					</div>
+		<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+		<h1 class="text-center">My Profile</h1>
+		<div class="row">
+	 <body>
+  <table class="table">
+
+    <tbody>
+      <tr>
+        <td>Nama</td>
+        <td>:</td>
+        <td>Novy Dhatika Pramikasuri</td>
+      </tr>
+      <tr>
+       <td>NIM</td>
+        <td>:</td>
+        <td>1541180117</td>
+      </tr>
+      <tr>
+        <td>TTL</td>
+        <td>:</td>
+        <td>Malang, 30 November 1996</td>
+      </tr>
+      <tr>
+        <td>No.Telp</td>
+        <td>:</td>
+        <td>081 351 476 299</td>
+      </tr>
+      <tr>
+        <td>Hobby</td>
+        <td>:</td>
+        <td>Main Basket, berenang, dengarkan musik, buat lagu, nyanyi dll</td>
+      </tr>
+      <tr>
+      <td>Alamat</td>
+        <td>:</td>
+        <td>jl. Arjuno 3 / 1146 Malang</td>
+        </tr>
+    </tbody>
+  </table>
+</body>
+
+
+		<!-- jQuery -->
+		<script src="//code.jquery.com/jquery.js"></script>
+		<!-- Bootstrap JavaScript -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+		<!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+ 		<script src="Hello World"></script>
+	</body>
+</html>
